d540705313a5e4e25762e9e867d8b9753d183226
Jan Kwinta <94843940+KwintaJ@users.noreply.github.com>
2022-12-16 11:24:14 +0100

    Add files via upload

---
+++ Projekt/Generator.cpp
@@ -0,0 +1,22 @@
+//############################################//
+//                                            //
+//    Jan Kwinta                xx.xx.2023    //
+//                                            //
+//                                            //
+//    Projekt: Prostokat                      //
+//    Plik: Generator.cpp                     //
+//                                            //
+//    Program do generowania danych           //
+//    wejsciowych                             //
+//                                            //
+//############################################//
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL); std::cout.tie(NULL);
+    
+    std::cout << "gen" << std::endl;
+}
---
+++ Projekt/Tester.cpp
@@ -0,0 +1,21 @@
+//############################################//
+//                                            //
+//    Jan Kwinta                xx.xx.2023    //
+//                                            //
+//                                            //
+//    Projekt: Prostokat                      //
+//    Plik: Generator.cpp                     //
+//                                            //
+//    Program do testowania                   //
+//                                            //
+//############################################//
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL); std::cout.tie(NULL);
+    
+    std::cout << "test" << std::endl;
+}
