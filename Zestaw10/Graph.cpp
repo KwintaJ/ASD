1383d9586cf641639ccd4f4741100b54eafc5c74
Jan Kwinta <94843940+KwintaJ@users.noreply.github.com>
2023-03-14 16:20:46 +0100

    Add files via upload

---
+++ Zestaw10/Graph.cpp
@@ -0,0 +1,37 @@
+//############################################//
+//                                            //
+//    Jan Kwinta                28.03.2023    //
+//                                            //
+//                                            //
+//    Zestaw10: Implementacja grafu           //
+//                                            //
+//    Plik: Graph.cpp                         //
+//                                            //
+//############################################//
+
+#include "Graph.hpp"
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL); std::cout.tie(NULL);
+
+    Graph<int> Graf;
+
+    /* ------ wejscie ------ */
+
+    int n;
+    std::cin >> n;
+    
+    char element;
+    while(n--)
+    {
+        std::cin >> element;
+        
+    }
+
+    /* ------ wyjscie ------ */
+    
+}
---
+++ Zestaw10/Graph.hpp
@@ -0,0 +1,44 @@
+//############################################//
+//                                            //
+//    Jan Kwinta                28.03.2023    //
+//                                            //
+//                                            //
+//    Zestaw10: Implementacja grafu           //
+//                                            //
+//    Plik: Graph.hpp                         //
+//                                            //
+//############################################//
+
+#include <stdexcept>
+#include <algorithm>
+#include <iostream>
+
+#ifndef GRAPH
+#define GRAPH
+
+template<class T>
+struct Vertex
+{
+    int priority;
+    T val;
+};
+
+template<class T>
+class Graph
+{    
+public:
+    Graph()
+    {
+        
+    }
+
+    ~Graph()
+    {
+        
+    }    
+    
+private:
+    
+};
+
+#endif
