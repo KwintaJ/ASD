05187fe696ba148c8c15225079848f1b989afe73
Jan Kwinta <94843940+KwintaJ@users.noreply.github.com>
2023-04-13 23:03:49 +0200

    Add files via upload

--- Zestaw10/Graph.cpp
+++ Zestaw10/Graph.cpp
@@ -1,6 +1,6 @@
 //############################################//
 //                                            //
-//    Jan Kwinta                28.03.2023    //
+//    Jan Kwinta                13.04.2023    //
 //                                            //
 //                                            //
 //    Zestaw10: Implementacja grafu           //
@@ -12,26 +12,80 @@
 #include "Graph.hpp"
 
 #include <iostream>
+#include <set>
 
 int main(int argc, char **argv)
 {
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(NULL); std::cout.tie(NULL);
 
-    Graph<int> Graf;
+    unsigned int s, n;
+    std::cin >> s >> n;
 
-    /* ------ wejscie ------ */
+    Graph Graf(s);
 
-    int n;
-    std::cin >> n;
+    /* ------ operacje ------ */
     
-    char element;
+    char operation;
+    int u, v;
+    std::set<int> conn;
     while(n--)
     {
-        std::cin >> element;
-        
+        std::cin >> operation;
+
+        switch(operation)
+        {
+            case 'A':
+                std::cin >> u >> v;
+                Graf.addEdge(u, v);
+                break;
+
+            case 'R':
+                std::cin >> u >> v;
+                Graf.removeEdge(u, v);
+                break;
+
+            case 'H':
+                std::cin >> u >> v;
+                std::cout << "Pomiedzy wierzcholkami " << u << " i " << v;
+                std::cout << (Graf.hasEdge(u, v) ? " jest krawedz." : " nie ma krawedzi.") << std::endl;
+                break;
+
+            case 'I':
+                std::cin >> v;
+                conn = Graf.inConnections(v);
+                for(auto x : conn)
+                    std::cout << x << ", ";
+                std::cout << std::endl;
+                conn.clear();
+                break;
+
+            case 'O':
+                std::cin >> v;
+                conn = Graf.outConnections(v);
+                for(auto x : conn)
+                    std::cout << x << ", ";
+                std::cout << std::endl;
+                conn.clear();
+                break;
+
+            case 'C':
+                std::cin >> v;
+                conn = Graf.allConnections(v);
+                for(auto x : conn)
+                    std::cout << x << ", ";
+                std::cout << std::endl;
+                conn.clear();
+                break;
+        }
     }
 
-    /* ------ wyjscie ------ */
+    /* ------ BFS ------ */
+
+    std::vector<int> pathLens = Graf.BFS(6);
+    for(auto x : pathLens)
+        std::cout << x << "  ";
+    std::cout << std::endl;
+
     
 }
--- Zestaw10/Graph.hpp
+++ Zestaw10/Graph.hpp
@@ -1,6 +1,6 @@
 //############################################//
 //                                            //
-//    Jan Kwinta                28.03.2023    //
+//    Jan Kwinta                13.04.2023    //
 //                                            //
 //                                            //
 //    Zestaw10: Implementacja grafu           //
@@ -12,33 +12,119 @@
 #include <stdexcept>
 #include <algorithm>
 #include <iostream>
+#include <set>
+#include <vector>
+#include <queue>
 
 #ifndef GRAPH
 #define GRAPH
 
-template<class T>
-struct Vertex
-{
-    int priority;
-    T val;
-};
 
-template<class T>
 class Graph
 {    
 public:
-    Graph()
+    Graph(unsigned int numVert)
     {
-        
+        graphSize = numVert;
+        array = std::vector<std::set<int>>(numVert);
     }
 
-    ~Graph()
+    void addEdge(int i, int j)
     {
+        array[i].insert(j);
+    }
+
+    void removeEdge(int i, int j)
+    {
+        auto jthIndex = array[i].find(j);
+        if(jthIndex != array[i].end())
+            array[i].erase(jthIndex);
+    }
+
+    bool hasEdge(int i, int j)
+    {
+        return array[i].find(j) != array[i].end();
+    }
+
+    std::set<int> inConnections(int i)
+    {
+        std::set<int> S;
         
-    }    
+        int k = 0;
+        for(auto C : array)
+        {
+            if(C.find(i) != C.end())
+            {
+                S.insert(k);
+            }
+            k++;
+        }
+
+        return S;
+    }
+
+    std::set<int> outConnections(int i)
+    {
+        return array[i];
+    }
+
+    std::set<int> allConnections(int i)
+    {
+        std::set<int> S(array[i]);
+
+        int k = 0;
+        for(auto C : array)
+        {
+            if(C.find(i) != C.end())
+            {
+                S.insert(k);
+            }
+            k++;
+        }
+
+        return S;
+    }
+
+    void print()
+    {
+        int k = 0;
+        for(auto C : array)
+        {
+            std::cout << k << ":" << std::endl;
+            for(auto x : C)
+                std::cout << "    " << x << ", ";
+            std::cout << std::endl << std::endl;
+            k++;
+        }
+    }
+
+    std::vector<int> BFS(int v)
+    {
+        std::vector<int> Lens(graphSize, -1);
+        Lens[v] = 0;
+
+        std::queue<int> Q;
+        int currentV = v;
+        do{
+            for(auto u : array[currentV])
+            {
+                if(Lens[u] == -1)
+                {
+                    Q.push(u);
+                    Lens[u] = Lens[currentV] + 1;
+                }
+            }
+            currentV = Q.front();
+            Q.pop();
+        }
+        while(!Q.empty());
+
+        return Lens;
+    }
     
 private:
-    
+    int graphSize;
+    std::vector<std::set<int>> array;
 };
 
 #endif
